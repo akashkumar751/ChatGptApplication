def39f06bf1de85cfd67a672de50d5e1c690b158
Akash Kumar <53569572+Akash751@users.noreply.github.com>
2023-07-22 18:42:10 +0530

    Add files via upload

---
+++ ChatGptApplication/src/main/java/com/chat/gpt/api/integration/ChatGptApplication.java
@@ -0,0 +1,13 @@
+package com.chat.gpt.api.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatGptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatGptApplication.class, args);
+	}
+
+}
---
+++ ChatGptApplication/src/test/java/com/chat/gpt/api/integration/ChatGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.gpt.api.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatGptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
